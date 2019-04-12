0c719236c21ad5f585a34f67eb61f5117676f9db
Prishu Panday <30753218+prishee@users.noreply.github.com>
2019-04-12 13:58:27 +0530

    Update keras-house_price_prediction.py

--- keras-house_price_prediction.py
+++ keras-house_price_prediction.py
@@ -1,9 +1,3 @@
-#
-#   house_price_prediction.py
-#
-#   This is a very simple prediction of house prices based on house size, implemented
-#   in Tensorflow. 
-
 import tensorflow as tf
 import numpy as np
 import math
